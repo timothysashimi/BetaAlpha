9995d18e8165333627bed77b23fbf478971375c1
intimodating <timothygenius@gmail.com>
2023-07-02 17:32:22 +0800

    added camera function

--- Flutter/lib/pages/upload_video/gallery.dart
+++ Flutter/lib/pages/upload_video/gallery.dart
@@ -3,7 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:orbital_app/components/video_data_server.dart';
 import 'upload_video_page.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
-
+import 'camera_screen.dart';
+ 
 class GalleryPage extends StatefulWidget {
   @override
   _GalleryPage createState() => _GalleryPage();
@@ -41,57 +42,71 @@ class _GalleryPage extends State<GalleryPage> {
     // TODO: implement build
     return Scaffold(
         appBar: AppBar(title: Text("Gallery"), bottom: getSearchBar()),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.cloud),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => UploadVideoPage()));
-          },
-        ),
-        body: FutureBuilder(
-            future: list,
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasError)
-                return new Text('Error: ${snapshot.error}');
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return new LinearProgressIndicator(
-                    backgroundColor: Colors.deepPurpleAccent,
-                  );
-                default:
-                  if (_search == true) {
-                    return GridView.count(
-                        crossAxisCount: 2,
-                        children:
-                            List<Widget>.generate(_searchList.length, (index) {
-                          return GridVideo(
-                            _searchList[index].title,
-                            _searchList[index].description,
-                            _searchList[index].userID,
-                            _searchList[index].uri,
-                          );
-                        }));
-                  } else {
-                    return GridView.count(
-                        crossAxisCount: 2,
-                        children: List<Widget>.generate(snapshot.data.length,
-                            (index) {
-                          aux_list.add(VideoDataServer(
-                            snapshot.data[index].title,
-                            snapshot.data[index].description,
-                            snapshot.data[index].userID,
-                            snapshot.data[index].uri,
-                          ));
-                          return GridVideo(
-                            snapshot.data[index].title,
-                            snapshot.data[index].description,
-                            snapshot.data[index].userID,
-                            snapshot.data[index].uri,
-                          );
-                        }));
+
+        body: Column(
+          children: [
+            const SizedBox(height: 450),
+            FloatingActionButton(
+              child: Icon(Icons.cloud),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => UploadVideoPage()));
+              },
+            ),
+            const SizedBox(height: 30),
+            FloatingActionButton(
+              child: const Icon(Icons.camera),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CameraScreen()));
+              },
+            ),
+            FutureBuilder(
+                future: list,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasError)
+                    return new Text('Error: ${snapshot.error}');
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return new LinearProgressIndicator(
+                        backgroundColor: Colors.deepPurpleAccent,
+                      );
+                    default:
+                      if (_search == true) {
+                        return GridView.count(
+                            crossAxisCount: 2,
+                            children:
+                                List<Widget>.generate(_searchList.length, (index) {
+                              return GridVideo(
+                                _searchList[index].title,
+                                _searchList[index].description,
+                                _searchList[index].userID,
+                                _searchList[index].uri,
+                              );
+                            }));
+                      } else {
+                        return GridView.count(
+                            crossAxisCount: 2,
+                            children: List<Widget>.generate(snapshot.data.length,
+                                (index) {
+                              aux_list.add(VideoDataServer(
+                                snapshot.data[index].title,
+                                snapshot.data[index].description,
+                                snapshot.data[index].userID,
+                                snapshot.data[index].uri,
+                              ));
+                              return GridVideo(
+                                snapshot.data[index].title,
+                                snapshot.data[index].description,
+                                snapshot.data[index].userID,
+                                snapshot.data[index].uri,
+                              );
+                            }));
+                      }
                   }
-              }
-            }));
+                }),
+          ],
+        ));
   }
 
   getSearchBar() {

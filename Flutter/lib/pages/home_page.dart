04dc6aa1627fc42c4dcd17577a608d57756ffab0
chewbum <e0958165@u.nus.edu>
2023-07-21 10:56:18 +0800

    final changes to api

--- Flutter/lib/pages/home_page.dart
+++ Flutter/lib/pages/home_page.dart
@@ -10,6 +10,8 @@ import 'package:orbital_app/pages/calendar.dart';
 import 'package:orbital_app/pages/home_page_widgets/line_chart_widget.dart';
 import 'package:orbital_app/pages/training_prog/training_screen.dart';
 import 'package:orbital_app/pages/google_mlkit/pose_dectector.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
 import 'package:path_provider/path_provider.dart';
 
 class Quote {
@@ -19,10 +21,94 @@ class Quote {
   Quote({required this.text, required this.author});
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+  late Quote _quote;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAndStoreQuote(); // Fetch quote initially
+    // Schedule daily updates
+    Workmanager().initialize(callbackDispatcher);
+    Workmanager().registerPeriodicTask(
+      '1',
+      'daily_quote_task',
+      inputData: <String, dynamic>{}, // You can pass any data if needed
+      frequency: const Duration(days: 1), // Repeat every 24 hours
+    );
+  }
+
+  Future<void> fetchQuoteIfNeeded() async {
+    final prefs = await SharedPreferences.getInstance();
+    final lastQuoteDate = prefs.getString('lastQuoteDate');
+    final currentDate = DateTime.now();
+
+    // Check if a new quote should be fetched (e.g., if it has been more than 24 hours since the last fetch)
+    if (lastQuoteDate == null ||
+        currentDate.difference(DateTime.parse(lastQuoteDate)).inHours >= 24) {
+      setState(() {
+        fetchAndStoreQuote();
+      });
+    }
+  }
+
+  Future<void> fetchAndStoreQuote() async {
+    try {
+      final fetchedQuote = await fetchRandomQuote();
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('lastQuoteDate', DateTime.now().toIso8601String());
+      prefs.setString('quoteText', fetchedQuote.text);
+      prefs.setString('quoteAuthor', fetchedQuote.author);
+      setState(() {
+        _quote = fetchedQuote;
+      });
+    } catch (error) {
+      // Handle error if fetching quote fails
+      print('Error fetching quote: $error');
+    }
+  }
+
+  Future<Quote> fetchRandomQuote() async {
+    final category = 'fitness';
+    final apiURL =
+        Uri.parse('https://api.api-ninjas.com/v1/quotes?category=$category');
+    final headers = {'X-Api-Key': 'ddVo/f1TaLq0sU3khWauMA==wkSTvHEgFDufZWl7'};
+
+    final response = await http.get(apiURL, headers: headers);
+    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      final quoteData = jsonDecode(response.body);
+      print(quoteData);
+      if (quoteData is List && quoteData.isNotEmpty) {
+        final quoteMap = quoteData[0] as Map<String, dynamic>;
+        final quote = quoteMap['quote'] as String;
+        final author = quoteMap['author'] as String;
+        final fetchedQuote = Quote(text: quote, author: author);
+        return fetchedQuote;
+      } else {
+        throw Exception('No quotes available for the given category');
+      }
+    } else {
+      throw Exception(
+          'Failed to fetch random quote. Error: ${response.statusCode}');
+    }
+  }
+
+  void callbackDispatcher() {
+    fetchAndStoreQuote();
+    Workmanager().executeTask((task, inputData) {
+      return Future.value(true);
+    });
+  }
 
   //sign user out method
   void signUserOut() {
@@ -179,7 +265,6 @@ class HomePage extends StatelessWidget {
               const SizedBox(height: 30),
               Column(
                 children: [
-                  const SizedBox(height: 20),
                   const Text(
                     "Quote of the day:",
                     style: TextStyle(
@@ -189,16 +274,42 @@ class HomePage extends StatelessWidget {
                       color: Color.fromARGB(255, 100, 98, 98),
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Winners train, losers complain",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      color: Color.fromARGB(255, 100, 98, 98),
-                    ),
-                    textAlign: TextAlign.center,
+                  FutureBuilder<Quote>(
+                    future: fetchRandomQuote(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator(); // Display a loading indicator while fetching the quote.
+                      } else if (snapshot.hasError) {
+                        print(snapshot.error);
+                        return Text(
+                          'When the muscles fail, the mind takes over',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                            color: Color.fromARGB(255, 100, 98, 98),
+                          ),
+                          textAlign: TextAlign.center,
+                        ); // Display an error message if fetching the quote fails.
+                      } else {
+                        final quote = snapshot
+                            .data; // The fetched quote will be available in snapshot.data.
+                        return Container(
+                          padding: EdgeInsets.all(
+                              20.0), // Adjust the padding as needed
+                          child: Text(
+                            quote!.text,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              color: Color.fromARGB(255, 100, 98, 98),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      }
+                    },
                   ),
                 ],
               ),
--- Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import flutter_local_notifications
 import flutter_native_timezone_updated_gradle
 import package_info_plus
 import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import wakelock_plus
@@ -28,6 +29,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
